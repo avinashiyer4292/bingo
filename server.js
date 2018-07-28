34b97c7ad6d7f394273ce4b9a6ad6252e0a7c25c
avinashiyer <avinashiyer4292@gmail.com>
2018-07-28 14:31:52 -0700

    Added path module

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app =express();
+const path = require('path');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const apis = require('./apis.js');
