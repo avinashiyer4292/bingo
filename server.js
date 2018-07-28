3ba8ed5e5429bc719278efd4d6d21bd9075c4073
avinashiyer <avinashiyer4292@gmail.com>
2018-07-28 14:42:25 -0700

    Fixed build path

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const apis = require('./apis.js');
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../react-client/build')));
+    app.use(express.static(path.join(__dirname, 'react-client/build')));
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, '../react-client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'react-client/build', 'index.html'));
     });
   }
 
