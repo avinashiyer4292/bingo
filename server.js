1a30290b4fe70828a46cb256f31bcdfe2fb9b67b
avinashiyer <avinashiyer4292@gmail.com>
2018-07-28 12:21:24 -0700

    Updated server scripts

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const port = 5000;
 const apis = require('./apis.js');
 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'react-client/build')));
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'react-client/build', 'index.html'));
+    });
+  }
+
 app.use(bodyParser.urlencoded({
     extended : false
  }));
