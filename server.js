0c6d24366dae2ba3204a5869f45e8ae46d83e352
avinashiyer <avinashiyer4292@gmail.com>
2018-07-28 12:36:40 -0700

    Updated server prod scripts

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const apis = require('./apis.js');
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'react-client/build')));
+    app.use(express.static(path.join(__dirname, '../react-client/build')));
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'react-client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../react-client/build', 'index.html'));
     });
   }
+
 
 app.use(bodyParser.urlencoded({
     extended : false
