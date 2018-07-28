d5fa49105e49331ef9c19067d44b5a43c13ce2cb
avinashiyer <avinashiyer4292@gmail.com>
2018-07-28 14:28:45 -0700

    Fixed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app =express();
 const bodyParser = require('body-parser');
-const port = 5000;
+const port = process.env.PORT || 5000;
 const apis = require('./apis.js');
 
 
